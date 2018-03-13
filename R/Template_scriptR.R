b23c9d19523fa199e0cf9a8d35cef7f64d7b9068
Pious79 <pierrelhermite@yahoo.fr>
2018-03-13 10:42:18 +0100

    Commit

---
+++ R/Template_scriptR.R
@@ -0,0 +1,25 @@
+#'Short resume of function (max 10 words)
+#'
+#'Explain the function
+#'
+#'@param  name of the parameter 1 and details
+#'@param  name of the parameter 2 and details
+#'
+#'@return value of output 1
+#'@return value of output 2
+#'
+#'@author Florine Garcia (florine.garcia@gmail.com)
+#'@author Pierre L'Hermite (pierrelhermite@yahoo.fr)
+#'
+#'@examples
+#'How to use function
+#'
+#'@references
+#'
+#'@details
+#'
+#'@seealso
+#'\code{\link[package name]{function name}}
+
+function_name <- function(data) {
+}
