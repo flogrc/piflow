6a9fbb89ba8eb17a7d7c6fb77fa10bdd653af59e
Pious79 <pierrelhermite@yahoo.fr>
2018-03-19 17:43:18 +0100

    Headings in roxygen version

--- R/daily2annual.R
+++ R/daily2annual.R
@@ -1,36 +1,37 @@
-##____________________________________________________________________________##
-##  Function to calculate annual data from daily data                         ##
-##  Florine Garcia - 20150520 - daily2annual                                  ##
-##____________________________________________________________________________##
-##----------------------------------------------------------------------------##
-#   Function : Transform a daily regular time series into an annual one with
-#     the threshold of missing values authorized per year given by the user and
-#     the choice of the day to start the year (hydrological or not)
-##----------------------------------------------------------------------------##
-#   Arguments : dailyData [zoo] : zoo object with the daily time series (date
-#                 format: "%Y-%m-%d")
-#               startYear [character]: indicate the start of the hydrological
-#                 year the fate format "MM-01" (default: calendar year, "01-01")
-#               FUN [function] : function that have to be applied for the annual
-#                 aggregation (by default mean)
-#               threshold [numeric] : threshold of missing values authorized to
-#                 compute the annual aggregation. Values must be between 0 and 1
-#                 (by default 10%: 0.1)
-##----------------------------------------------------------------------------##
-#   Sortie : AnnualData [zoo] : zoo object with the annual computed time series
-#              (date in the format "%Y-MM-01", corresponding of the start of the
-#              year)
-##----------------------------------------------------------------------------##
-##----------------------------------------------------------------------------##
-#   WARNINGS : Fonction modifiee le 20170802 par Florine Garcia :
-#                - ajout option calcul annee hydro
-#                - Adi calcule dans la fonction
-#     !!!!! Meme format de sortie quelque soit le type de calcul. Exemple : Pour
-#     les annees civiles, sortie 1959-01-01 correspond au calcul du 01/01/1959
-#     au 31/12/1959. Pour une annee hydro demarrant le 01/08, sortie 1959-01-01
-#     correspond au calcul du 01/08/1958 au 31/07/1959.
-##----------------------------------------------------------------------------##
-#-------------------------------------------------------------------------------
+#'Function to calculate annual data from daily data
+#'
+#'Transform a daily regular time series into an annual one with
+#'the threshold of missing values authorized per year given by the user and
+#'the choice of the day to start the year (hydrological or not)
+#'
+#'@param  dailyData [zoo] zoo object with the daily time series (date format:
+#'"\%Y-\%m-\%d")
+#'@param  startYear [character] indicate the start of the hydrological
+#'year the fate format "MM-01" (default: calendar year, "01-01")
+#'@param  FUN [function] function that have to be applied for the annual
+#'aggregation (by default mean)
+#'@param threshold [numeric] : threshold of missing values authorized to
+#'compute the annual aggregation. Values must be between 0 and 1
+#'(by default 10\%: 0.1)
+#'
+#'@return \emph{AnnualData} [zoo] : zoo object with the annual computed time series
+#'(date in the format "\%Y-MM-01", corresponding of the start of the year)
+#'
+#'@author Florine Garcia (florine.garcia@gmail.com)
+#'@author Pierre L'Hermite (pierrelhermite@yahoo.fr)
+#'
+#'@examples
+#'daily2annual(dailyprec, startYear = "08-01", FUN = sum, threshold = 0.1)
+#'
+#'@details
+#'WARNINGS : Fonction modifiee le 20170802 par Florine Garcia :
+#'- ajout option calcul annee hydro
+#'- Adi calcule dans la fonction
+#'!!!!! Meme format de sortie quelque soit le type de calcul. Exemple : Pour
+#'les annees civiles, sortie 1959-01-01 correspond au calcul du 01/01/1959
+#'au 31/12/1959. Pour une annee hydro demarrant le 01/08, sortie 1959-01-01
+#'correspond au calcul du 01/08/1958 au 31/07/1959.
+
 daily2annual <- function(dailyData, startYear = "01-01", FUN = mean,
                          threshold = 0.1) {
   ##__Check_Input_Arguments_________________________________________________####
