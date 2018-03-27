8030be81f4a5c6564286a642131b3e023b84df3a
Florine Garcia <florine.garcia@gmail.com>
2018-03-27 09:32:37 +0200

    delete the for, faster function! + doc

--- R/yearTranslation.R
+++ R/yearTranslation.R
@@ -6,39 +6,54 @@
 #   Description: creation of a vector years translated to compute statistics on
 #                  hydrological years
 ##----------------------------------------------------------------------------##
-#   Arguments: tsData [zoo]: zoo object with the daily or monthly time series
-#                (date format: "%Y-%m-%d")
-#              startYear [character]: indicate the start of the hydrological
-#                 year the fate format "MM-01" (default: calendar year, "01-01")
+#   Arguments: tsData [zoo]: zoo object with the daily or monthly time series.
+#                The date format must be:
+#                  - "%Y-%m-%d" for daily data
+#                  - "%Y-%m-01" for monthly data
+#              startYear [character]: indicate the month to start the
+#                hydrological year. The format must be "MM" (by default, 
+#                calendar year: "01")
 ##----------------------------------------------------------------------------##
 #   Value: years [numeric]: vector of the translated years
 ##----------------------------------------------------------------------------##
 #-------------------------------------------------------------------------------
-yearTranslation <- function(tsData, startYear, ...) {
+yearTranslation <- function(tsData, startYear) {
   ##__Check_Input_Arguments_________________________________________________####
   # --- Check the class
   if (!is.zoo(tsData)) { stop("tsData must be a zoo"); return(NULL) }
   if (!is.character(startYear)) {
     stop("startDate must be a character"); return(NULL)
   }
+  # --- Check the time step and the date format
+  if (!(periodicity(tsData)$scale %in% c("daily", "monthly"))) {
+    stop("dailyData must be a daily or monthly time series \n"); return(NULL)
+  }
+  # --- Check the start of the year
+  if (nchar(startYear) != 2) {
+    stop("startYear format must be 'MM'"); return(NULL)
+  }
   
   ##__year_translation______________________________________________________####
   dates <- time(tsData)
   y <- as.numeric(format(dates[1], "%Y"))
-  startDate <- paste0(y, "-", startYear)
-  
+  startDate <- paste0(y, "-", startYear, "-01")
+  # --- dates before start
   if (dates[1] < startDate) {
-    iyear <- y - 1
-  } else {
-    iyear <- as.numeric(format(dates[1], "%Y"))
+    datesBefore <- dates[1:(which(dates == startDate) - 1)]
+    yBefore <- rep((y - 1), length(datesBefore)
+    dates <- dates[which(dates == startDate):length(dates)]
   }
+  # --- translation
+  timestep <- periodicity(tsData)$scale
+  switch(timestep,
+         "daily" = { tstp <- "days" },
+         "monthly" = { tstp <- "months" }, {
+           stop("unkown value for choice of timestep: ", timestep, "\n")
+         })
+  years <- format(seq(startDate, by = tstp, length. = length(dates)), "%Y")
   
-  years <- c(iyear, rep(NA, (length(dates) - 1)))
-  for (id in 2:length(dates)) {
-    if (format(dates[id], "%m-%d") == startYear) {
-      iyear <- iyear + 1
-    }
-    years[id] <- iyear
+  if (exists("yBefore")) {
+    years <- c(yBefore, as.numeric(years))
   }
   
   return(years)
