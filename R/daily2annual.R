a2d348c83877711d9085d658e48c526c5a686c4f
Florine Garcia <florine.garcia@gmail.com>
2018-03-27 09:31:29 +0200

    Change of startYear + doc

--- R/daily2annual.R
+++ R/daily2annual.R
@@ -3,36 +3,27 @@
 ##  Florine Garcia - 20150520 - daily2annual                                  ##
 ##____________________________________________________________________________##
 ##----------------------------------------------------------------------------##
-#   Function : Transform a daily regular time series into an annual one with
-#     the threshold of missing values authorized per year given by the user and
-#     the choice of the day to start the year (hydrological or not)
+#   Function : Transform a daily regular time series into an annual one. The
+#     user can give a threshold of missing values authorized per year for the
+#     aggregation and choose the day to start the year (hydrological or not)
 ##----------------------------------------------------------------------------##
-#   Arguments : dailyData [zoo] : zoo object with the daily time series (date
-#                 format: "%Y-%m-%d")
-#               startYear [character]: indicate the start of the hydrological
-#                 year the fate format "MM-01" (default: calendar year, "01-01")
-#               FUN [function] : function that have to be applied for the annual
-#                 aggregation (by default mean)
-#               threshold [numeric] : threshold of missing values authorized to
-#                 compute the annual aggregation. Values must be between 0 and 1
-#                 (by default 10%: 0.1)
+#   Arguments: dailyData [zoo] : zoo object with the daily time series. The
+#                date format must be "%Y-%m-%d"
+#              startYear [character]: indicate the month to start the
+#                hydrological year. The format must be "MM" (by default, 
+#                calendar year: "01")
+#              FUN [function] : function that have to be applied for the annual
+#                aggregation (by default: mean)
+#              threshold [numeric] : threshold of missing values authorized to
+#                compute the annual aggregation. Values must be between 0 and 1
+#                (by default, 10%: 0.1)
 ##----------------------------------------------------------------------------##
-#   Sortie : AnnualData [zoo] : zoo object with the annual computed time series
-#              (date in the format "%Y-MM-01", corresponding of the start of the
-#              year)
-##----------------------------------------------------------------------------##
-##----------------------------------------------------------------------------##
-#   WARNINGS : Fonction modifiee le 20170802 par Florine Garcia :
-#                - ajout option calcul annee hydro
-#                - Adi calcule dans la fonction
-#     !!!!! Meme format de sortie quelque soit le type de calcul. Exemple : Pour
-#     les annees civiles, sortie 1959-01-01 correspond au calcul du 01/01/1959
-#     au 31/12/1959. Pour une annee hydro demarrant le 01/08, sortie 1959-01-01
-#     correspond au calcul du 01/08/1958 au 31/07/1959.
+#   Sortie : annualData [zoo] : zoo object with the annual computed time series.
+#              The date is in the format "%Y-MM-01", corresponding of the start
+#              of the year
 ##----------------------------------------------------------------------------##
 #-------------------------------------------------------------------------------
-daily2annual <- function(dailyData, startYear = "01-01", FUN = mean,
-                         threshold = 0.1) {
+daily2annual <- function(dailyData, startYear = "01", FUN = mean, threshold = 0.1) {
   ##__Check_Input_Arguments_________________________________________________####
   # --- Check the class
   if (!is.zoo(dailyData)) { stop("dailyData must be a zoo"); return(NULL) }
@@ -43,18 +34,17 @@ daily2annual <- function(dailyData, startYear = "01-01", FUN = mean,
   if (!is.numeric(threshold)) {
     stop("threshold must be a numeric"); return(NULL)
   }
-  # --- Check the time step
+  # --- Check the time step and the date format
   if (periodicity(dailyData)$scale != "daily") {
     stop("dailyData must be a daily time series \n"); return(NULL)
   }
   # --- Check the start of the year
-  if (substr(startYear, 4, 5) != "01") {
-    stop("startYear format must be 'MM-01'"); return(NULL)
+  if (nchar(startYear) != 2) {
+    stop("startYear format must be 'MM'"); return(NULL)
   }
   # --- Check the threshold value
   if (threshold > 1) {
-    threshold <- threshold / 100
-    warning("threshold was > 1 and then divided by 100")
+    stop("threshold must be < 1")
   }
   
   ##__Time_series_extension_and_Time_translate______________________________####
