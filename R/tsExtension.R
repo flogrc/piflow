ac6e48015d2999744313c2e1506ecc34cfc6c436
Florine Garcia <florine.garcia@gmail.com>
2018-03-27 23:07:57 +0200

    delete unused variables

--- R/tsExtension.R
+++ R/tsExtension.R
@@ -49,7 +49,7 @@ tsExtension <- function(tsData, startDate = start(tsData), endDate = end(tsData)
     if (format(endDate, "%d") != "01") {
       stop("format of 'endDate' must be '%Y-%m-01'")
     }
-    if (sum(as.numeric(format(time(tsData), "%d"))-1) != 0) {
+    if (sum(as.numeric(format(time(tsData), "%d")) - 1) != 0) {
       stop("format of 'tsData' must be '%Y-%m-01'")
     }
   }
@@ -62,8 +62,6 @@ tsExtension <- function(tsData, startDate = start(tsData), endDate = end(tsData)
            stop("unkown value for choice of timestep: ", timestep, "\n")
          })
   # --- Ts extension
-  dates <- time(tsData)
-  y <- as.numeric(format(dates, "%Y"))
   seqDates <- seq(as.Date(startDate), as.Date(endDate), by = tstp)
   # --- NA fill preparation
   naDates <- zoo(rep(NA, length(seqDates)), as.Date(seqDates))
@@ -71,6 +69,6 @@ tsExtension <- function(tsData, startDate = start(tsData), endDate = end(tsData)
   # --- Merge for extension and filling
   extTsData <- merge.zoo(naDates, winTsData)
   extTsData <- extTsData[, -1]
-
+  
   return(extTsData)
 }
