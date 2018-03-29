64ce20387ae79da28b2e5ae6657b443b035f5ff2
Pious79 <pierrelhermite@yahoo.fr>
2018-03-29 17:50:14 +0200

    Starting headings and few modif in code

--- R/monthly2annual.R
+++ R/monthly2annual.R
@@ -1,27 +1,31 @@
-##____________________________________________________________________________##
-##  Function to calculate annual data from monthly data                       ##
-##  Florine Garcia - 20151221 - fc.monthly2annual                             ##
-##____________________________________________________________________________##
-##----------------------------------------------------------------------------##
-#   Fonction : Transformer les donnees mensuelles en donnees annuelles avec
-#     le niveau de lacunes autorisees donnes par l'utilisateur et choix de
-#     l'annee de calcul civile ou hydro
-##----------------------------------------------------------------------------##
-#   Arguments : monthlyData [zoo] : vecteur contenant chaque donnee par mois
-#                 (date au format "%Y-%m-%d")
-#               debYear [character]: debut de l'annee pour le calcul de la
-#                 moyenne au format "MM-01" (par defaut annee civile "01-01")
-#               FUN [function] : fonction a appliquer annuellement (par defaut
-#                 mean)
-#               na.rm [boolean] : vecteur booleen, argument de FUN (par defaut
-#                 TRUE)
-#               threshold [numeric] : seuil autorise pour la prise en compte des
-#                 lacunes (par defaut 10%)
-##----------------------------------------------------------------------------##
-#   Sortie : annualData [zoo] : vecteur contenant chaque donnee par annee (date
-#              au format "%Y-%m-%d")
-##----------------------------------------------------------------------------##
-##----------------------------------------------------------------------------##
+#'Calculate annual data from monthly data
+#'
+#'Transform a monthly time series into an annual one. The user can give a
+#' threshold of missing values authorized per year for the aggregation and choose
+#' the day to start the year (hydrological year or not)
+#'
+#'@param  monthlyData [zoo] zoo object with the monthly time series. The date
+#'format must be "\%Y-\%m-\%d"
+#'@param  FUN [function] function that have to be applied for the annual
+#'aggregation (by default: mean)
+#'@param  startYear [character] indicate the month to start the hydrological
+#'year. The format must be "MM-01" (by default, calendar year: "01")
+#'@param threshold [numeric] threshold of missing values authorized to compute
+#'the annual aggregation. Values must be between 0 and 1 (by default, 10\%: 0.1)
+#'@param na.rm [boolean] boolean vector (by default, TRUE)
+#'@param ... further arguments
+#'
+#'@return \emph{\code{annualData}} [zoo] : zoo object with the annual computed
+#'time series. The date is in the format "\%Y-MM-01", corresponding of the start
+#' month of the year
+#'
+#'@author Florine Garcia (florine.garcia@gmail.com)
+#'@author Pierre L'Hermite (pierrelhermite@yahoo.fr)
+#'
+#'@examples
+#'monthly2annual(monthlyprec, FUN = sum, startYear = "08-01", threshold = 0.1,
+#'na.rm =TRUE)
+
 #   WARNINGS : Fonction modifiee le 20170802 par Florine Garcia :
 #                - ajout option calcul annee hydro
 #                - Aai calcule dans la fonction
@@ -29,21 +33,21 @@
 #     les annees civiles, sortie 1959-01-01 correspond au calcul du 01/01/1959
 #     au 31/12/1959. Pour une annee hydro demarrant le 01/08, sortie 1959-01-01
 #     correspond au calcul du 01/08/1958 au 31/07/1959.
-##----------------------------------------------------------------------------##
-#-------------------------------------------------------------------------------
-monthly2annual <- function(monthlyData, debYear = "01-01", FUN = mean,
+
+monthly2annual <- function(monthlyData, startYear = "01-01", FUN = mean,
                            na.rm = TRUE, threshold = 0.1) {
-  ## Verification arguments d'entree
+  ##__Check_Input_Arguments_________________________________________________####
+  # --- Check the class
   if (!is.zoo(monthlyData)) { stop("monthlyData must be a zoo"); return(NULL) }
-  # --- Verification du pas de temps
+  # --- Checking time step
   if (sfreq(monthlyData) != "monthly") {
     stop("monthlyData must be a monthly serie \n"); return(NULL)
   }
-  if (!is.character(debYear)) {
-    stop("debYear must be a character"); return(NULL)
+  if (!is.character(startYear)) {
+    stop("startYear must be a character"); return(NULL)
   }
-  if (substr(debYear, 4, 5) != "01") {
-    stop("debYear format must be 'MM-01'"); return(NULL)
+  if (substr(startYear, 4, 5) != "01") {
+    stop("startYear format must be 'MM-01'"); return(NULL)
   }
   if (threshold > 1) {
     threshold <- threshold / 100
@@ -53,14 +57,14 @@ monthly2annual <- function(monthlyData, debYear = "01-01", FUN = mean,
   ## Translation si option annee hydro
   dates <- time(monthlyData)
   y <- as.numeric(format(dates, "%Y"))
-  if (debYear != "01-01") {
-    if (dates[1] != as.Date(paste0(y[1], "-", debYear))) {
-      if (dates[1] < as.Date(paste0(y[1], "-", debYear))) {
-        inday <- which(dates == as.Date(paste0(y[1], "-", debYear)))
+  if (startYear != "01-01") {
+    if (dates[1] != as.Date(paste0(y[1], "-", startYear))) {
+      if (dates[1] < as.Date(paste0(y[1], "-", startYear))) {
+        inday <- which(dates == as.Date(paste0(y[1], "-", startYear)))
         monthlyData <- monthlyData[inday:length(monthlyData)]
       }
-      if (dates[1] > as.Date(paste0(y[1], "-", debYear))) {
-        ldays <- seq(as.Date(paste0(y[1], "-", debYear)), dates[1],
+      if (dates[1] > as.Date(paste0(y[1], "-", startYear))) {
+        ldays <- seq(as.Date(paste0(y[1], "-", startYear)), dates[1],
                      by = "months")
         ldata <- zoo(rep(NA, length(ldays)-1), as.Date(ldays[-length(ldays)]))
         monthlyData <- c(ldata, monthlyData)
